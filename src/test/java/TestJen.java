425e7b0d298613244bd726694cbbdbaeeb8401c4
Vasili Savin <vasili_savin@epam.com>
2017-08-02 11:32:35 +0300

    Initial commit

---
+++ src/test/java/TestJen.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestJen {
+    @Test
+    public void theFirstTest(){
+        System.out.println("Hello, Jenkins");
+    }
+
+    @Test
+    public void theSecondTest(){
+        System.out.println("Hello, 2 tets");
+    }
+
+}
